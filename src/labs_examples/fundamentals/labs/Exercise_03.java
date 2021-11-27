5fb72a27bc8977957572968c6c694fb3f78fd828
shishidomingo <ericdomingo1@gmail.com>
2021-11-26 23:41:36 -0800

    completed Exercise 3

--- src/labs_examples/fundamentals/labs/Exercise_03.java
+++ src/labs_examples/fundamentals/labs/Exercise_03.java
@@ -15,6 +15,13 @@ public class Exercise_03 {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+        System.out.println("Check out my first program!");
+
+        int i = 10;
+        int x = 12;
+        int z = i + x;
+
+        System.out.println(z);
 
         // write code to print out "Check out my first program!" below
 
