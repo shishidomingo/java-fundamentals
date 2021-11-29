fbbb75643767a1e2ac80ed8071a1b4cfe3ede9b3
shishidomingo <ericdomingo1@gmail.com>
2021-11-28 20:56:08 -0800

    adding explicit casting to this practice session

--- src/labs_examples/fundamentals/labs/Casting.java
+++ src/labs_examples/fundamentals/labs/Casting.java
@@ -8,12 +8,28 @@ public class Casting {
 	float f = l;
 	double d = f;
 
+	System.out.println("casting via autoconversion: 6 (byte) > short > int > long > float > double");
 	System.out.println(b);
 	System.out.println(s);
 	System.out.println(i);
 	System.out.println(l);
 	System.out.println(f);
 	System.out.println(d);
+	System.out.println("And now in reverse from double to byte");
+
+	double d2 = 123.45;
+	float f2 = (float)d2;
+	long l2 = (long)f2;
+	int i2 = (int)l2;
+	short s2 = (short)i2;
+	byte b2 = (byte)s2;
+
+	System.out.println(d2);
+        System.out.println(f2);
+        System.out.println(l2);
+        System.out.println(i2);
+        System.out.println(s2);
+        System.out.println(b2);	
 
 	}
 }
