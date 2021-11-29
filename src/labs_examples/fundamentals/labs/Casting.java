11b16679d2d024f8a55416f07e635f134ce78480
shishidomingo <ericdomingo1@gmail.com>
2021-11-28 20:47:05 -0800

    practicing casting variables with auto-conversion

---
+++ src/labs_examples/fundamentals/labs/Casting.java
@@ -0,0 +1,20 @@
+public class Casting {
+	public static void main (String[] args){
+
+	byte b = 6;
+	short s = b;
+	int i = s;
+	long l = i;
+	float f = l;
+	double d = f;
+
+	System.out.println(b);
+	System.out.println(s);
+	System.out.println(i);
+	System.out.println(l);
+	System.out.println(f);
+	System.out.println(d);
+
+	}
+}
+
