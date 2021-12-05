21f04b4053411c41fafa020e1ccd202947b503fd
shishidomingo <ericdomingo1@gmail.com>
2021-12-04 21:51:22 -0800

    completed ex2-ex4 of inheritance

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
@@ -4,7 +4,9 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
  * Run the class below and study the output. What prints and why?
  *
  * Your answer:
- *
+ *  10 prints out.  the line A a = new B(); instantiates a new object called a, which is a member of class A.
+ *  since a member of class A has a integer i with a value of 10, a.i returns that value.  that it is instantiated
+ *  as a new B() is not relevant because that only gives object a access to the methods in class B.
  */
 class A {
     int i = 10;
--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_03.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_03.java
@@ -4,7 +4,9 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
  * Why does the output print in the order it does?
  *
  * You answer:
- *
+ *  in the psvm method, a new C_1 object called c is instantiated.  this calls the constructor class of C_1; which inherits
+ *  the constructor of B_1, which then inherits the constructor of A_1.  the order of execution of constructors starts from
+ *  the highest parent class and moves down to the lowest child class.  accordingly, A_1 executes first, then B_1, then C_1.
  */
 
 class A_1 {
